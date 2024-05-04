d7b726e3401b1cbac102829b8827d987f250eb84
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2024-05-04 16:02:08 +0200

    added audio control node and drag drop functionality

--- DragDropFileReader.gd
+++ DragDropFileReader.gd
@@ -1,10 +1,27 @@
 extends Panel
 
+@onready var trackname_lbl: Label = $TracknameLbl
+
+static var last_file_path: String
 
 func _ready() -> void:
-	get_tree().root.files_dropped.connect(_get_dropped_files)
+	get_tree().root.files_dropped.connect(_get_dropped_files) ## TODO: actually there needs to be only one connected signal, not all boxes...
 
 
 func _get_dropped_files(files: PackedStringArray) -> void:
+	last_file_path = files[0]
 	print_debug(files)
-	AudioPlayer.play_bgm_from_filename(files[0], AudioPlayer.PlaybackMode.PLAY_LOOPED)
+
+
+func _on_mouse_entered() -> void:
+
+	if last_file_path.is_empty(): return ## early exit
+	
+	
+	var filename_split: PackedStringArray = last_file_path.split("\\")
+	var filename: String = filename_split[filename_split.size() - 1]
+	trackname_lbl.text = filename
+	AudioPlayer.play_bgm_from_filename(last_file_path, AudioPlayer.PlaybackMode.PLAY_LOOPED)
+	
+	last_file_path = "" ## reset so it's not triggered again
+
